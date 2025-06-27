f1c06e92af33043fe639cec0254dd33266bdda50
jizzy-joey <ebubelewu@gmail.com>
2025-06-27 22:36:29 +0100

    fix: Correct method name in Button component for job routing

--- .next/static/chunks/_0e8c4987._.js
+++ .next/static/chunks/_0e8c4987._.js
@@ -20,7 +20,7 @@ function Button({ title }) {
     _s();
     const router = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useRouter"])();
     const handleClick = ()=>{
-        router.push(`/jobs/${title.toLowerCase().replace(/\s+/g, '-')}`);
+        router.push(`/jobs/${title.toLo().replace(/\s+/g, '-')}`);
     // This will navigate to the job description page with the title as a slug
     };
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("button", {
